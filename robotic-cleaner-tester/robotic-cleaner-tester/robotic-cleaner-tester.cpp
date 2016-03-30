71e00df9f6de517c9f927a2d1ebc6b120492b146
tuval10 <tuval10@gmail.com>
2016-03-31 01:43:15 +0300

    solved leak-memories

--- robotic-cleaner-tester/robotic-cleaner-tester/robotic-cleaner-tester.cpp
+++ robotic-cleaner-tester/robotic-cleaner-tester/robotic-cleaner-tester.cpp
@@ -5,21 +5,25 @@
 #include "RandomRobotAlgorithm.h"
 #include <time.h>
 #include <string.h>
+#include <stdlib.h>
+#define configFileName "config.ini"
+#define configFlag "-config"
+#define housePathFlag "-house_path"
 
 using namespace ns_robotic_cleaner_simulator;
 
 int main(int argc, char * argv[])
 {
 	srand((unsigned int)time(NULL));
-	char * configFilePath = (char *)("config.ini");
-	char * houseFolder = (char *)(".\\");
+	char * configFilePath = (char *)("");
+	char * houseFolder = (char *)("");
 	if(argc >= 3) 
 	{
-		if ( ! strcmp(argv[1], "–config"))
+		if ( ! strcmp(argv[1], configFlag))
 		{
 			configFilePath = argv[2];
 		}
-		else if (! strcmp(argv[1], "–house_path"))
+		else if (! strcmp(argv[1], housePathFlag))
 		{
 			houseFolder = argv[2];
 		}
@@ -30,11 +34,11 @@ int main(int argc, char * argv[])
 	}
 	if(argc == 5) 
 	{
-		if ( ! strcmp(argv[3], "–config"))
+		if ( ! strcmp(argv[3], configFlag))
 		{
 			configFilePath = argv[4];
 		}
-		else if ( ! strcmp(argv[3], "–house_path"))
+		else if ( ! strcmp(argv[3], housePathFlag))
 		{
 			houseFolder = argv[4];
 		}
@@ -43,10 +47,24 @@ int main(int argc, char * argv[])
 			cout << "invalid flags. flags are -config and -house_path";
 		}
 	}
-	Simulator s = Simulator(configFilePath);
-	s.LoadHouses(houseFolder);
+	std::string configFile(configFilePath);
+	if(strcmp(configFilePath, "")){
+		configFile.append("/");
+		configFile.append(configFileName);
+	}
+	else{
+		configFile.append(configFileName);
+	}
+	Simulator s = Simulator(configFile.c_str());
+	if(s.LoadHouses(houseFolder) == 0){
+		cout << "Cannot load house properly. exiting";
+		return EXIT_FAILURE;
+	}
+
 	s.LoadAlgorithmsAndRuns();
 	s.RunAll();
-	return 0;
+	//getchar();
+	return EXIT_SUCCESS;
 }
+
 
